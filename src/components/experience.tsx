cc200523b2ded70e3680c88ddeedaa0156af58f2
notkoyo <135956230+notkoyo@users.noreply.github.com>
2024-02-25 17:23:20 +0000

    add experience section, fix rendering bug

---
+++ src/components/experience.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
+import { useInView } from "react-intersection-observer";
+
+export default function Experience() {
+  const { ref } = useSectionInView("Experience");
+
+  return (
+    <section id="experience" ref={ref} className="mb-28 scroll-mt-28 sm:mb-40">
+      <SectionHeading>Experience</SectionHeading>
+      <VerticalTimeline lineColor="">
+        {experiencesData.map((item, i) => {
+          const { ref, inView } = useInView({
+            triggerOnce: true,
+          });
+          return (
+            <div key={i} ref={ref} className="vertical-timeline-element">
+              <VerticalTimelineElement
+                visible={inView}
+                contentStyle={{
+                  background: "#f3f4f6",
+                  boxShadow: "none",
+                  border: "1px solid rgba(0, 0, 0, 0.05)",
+                  textAlign: "left",
+                  padding: "1.3rem 2rem",
+                }}
+                contentArrowStyle={{
+                  borderRight: "0.4rem solid #9ca3af",
+                }}
+                date={item.date}
+                icon={item.icon}
+                iconStyle={{
+                  background: "white",
+                  fontSize: "1.5rem",
+                }}
+              >
+                <h3 className="font-semibold capitalize">{item.title}</h3>
+                <p className="!mt-0 font-normal">{item.location}</p>
+                <p>{item.description}</p>
+              </VerticalTimelineElement>
+            </div>
+          );
+        })}
+      </VerticalTimeline>
+    </section>
+  );
+}
