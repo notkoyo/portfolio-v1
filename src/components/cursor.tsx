b89027b5e89f2f08cee4a2d0c71319872631706d
notkoyo <135956230+notkoyo@users.noreply.github.com>
2024-02-25 02:48:17 +0000

    add custom cursor

---
+++ src/components/cursor.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React, { useEffect } from "react";
+import { motion, useMotionValue, useSpring } from "framer-motion";
+
+export default function Cursor() {
+  const cursorX = useMotionValue<number>(-100);
+  const cursorY = useMotionValue<number>(-100);
+
+  const springConfig = { damping: 45, stiffness: 550 };
+  const cursorXSpring = useSpring(cursorX, springConfig);
+  const cursorYSpring = useSpring(cursorY, springConfig);
+
+  useEffect(() => {
+    const moveCursor = (e: MouseEvent) => {
+      cursorX.set(e.clientX - 8);
+      cursorY.set(e.clientY - 8);
+    };
+
+    window.addEventListener("mousemove", moveCursor);
+
+    return () => {
+      window.removeEventListener("mousemove", moveCursor);
+    };
+  }, []); 
+
+  return (
+    <motion.div
+    className="fixed left-0 top-0 w-[1rem] h-[1rem] rounded-full mix-blend-difference z-[999] pointer-events-none bg-gray-50"
+    style={{
+      translateX: cursorXSpring,
+      translateY: cursorYSpring,
+    }}
+  />
+  );
+}
